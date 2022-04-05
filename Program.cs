c77c91923e4b3f382d098f9796c980c25822f90b
Yuto Ichinose <yuto.0526.sano@gmail.com>
2022-04-05 22:46:44 +0900

    初めて書いたC#

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, C#!");
+
+int x = 0;
+x = 2 + 3;
+Console.WriteLine(x);
