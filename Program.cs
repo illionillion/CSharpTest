227a153f505b2d219a62cf6613757e8c0c5baba5
Yuto Ichinose <yuto.0526.sano@gmail.com>
2022-04-06 10:16:32 +0900

    forとif

--- Program.cs
+++ Program.cs
@@ -5,3 +5,13 @@ Console.WriteLine("Hello, C#!");
 int x = 0;
 x = 2 + 3;
 Console.WriteLine(x);
+
+for (int i = 0; i <= x * 10; i++)
+{
+    if ( i%2 == 0)
+    {
+        Console.WriteLine(i+"は偶数");
+    }else{
+        Console.WriteLine(i+"は奇数");
+    }
+}
